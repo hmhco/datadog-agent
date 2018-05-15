88d584f84094151f524e6923c3ae92dd4d83c787
Maxime Mouial <maxime.mouial@gmail.com>
2018-05-15 17:16:49 -0400

    WIP

--- tasks/release.py
+++ tasks/release.py
@@ -10,7 +10,7 @@ from invoke import task, Failure
 def update_changelog(ctx, new_version):
     """
     Quick task to generate the new CHANGELOG using reno when releasing a minor
-    version.
+    version (linux only).
     """
     new_version_int = map(int, new_version.split("."))
 
@@ -18,12 +18,29 @@ def update_changelog(ctx, new_version):
         print("Error: invalid version: {}".format(new_version_int))
         raise Exit(1)
 
+    # let's avoid loosing uncommited change with 'git reset --hard'
+    try:
+        ctx.run("git diff --exit-code HEAD", hide="both")
+    except Failure as e:
+        print("Error: You have uncommited change, please commit or stash before using update_changelog")
+        return
+
     # let's check that the tag for the new version is present (needed by reno)
     try:
         ctx.run("git tag --list | grep {}".format(new_version))
     except Failure as e:
         print("Missing '{}' git tag: mandatory to use 'reno'".format(new_version))
         raise
+
+    # adding the Prelude
+    ctx.run("""echo 'prelude: >
+    - Please refer to the `{0} tag on integrations-core <https://github.com/DataDog/integrations-core/releases/tag/{0}>` for the list of changes on the Core Checks.
+
+    - Please refer to the `{0} tag on trace-agent <https://github.com/DataDog/datadog-trace-agent/releases/tag/{0}>` for the list of changes on the Trace Agent.
+
+    - Please refer to the `{0} tag on process-agent <https://github.com/DataDog/datadog-process-agent/releases/tag/{0}>` for the list of changes on the Process Agent.'\
+    | EDITOR=tee reno new prelude-release-{0} --edit""".format(new_version))
+    return
 
     # make sure we are up to date
     ctx.run("git fetch")
@@ -34,11 +51,6 @@ def update_changelog(ctx, new_version):
             | grep releasenotes/notes/`".format(previous_minor, previous_minor))
 
     # generate the new changelog
-    ctx.run("echo 'reno report \
-            --ignore-cache \
-            --earliest-version {}.0 \
-            --version {} \
-            --no-show-source > /tmp/new_changelog.rst'".format(previous_minor, new_version))
     ctx.run("reno report \
             --ignore-cache \
             --earliest-version {}.0 \
